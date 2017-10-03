578132806ddd8d1823386dad2d0d995c0d005b07
Greg Turnquist <gturnquist@pivotal.io>
2017-10-03 11:35:50 -0500

    Initial commit

---
+++ src/main/java/com/greglturnquist/links/EmployeeResourceProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.links;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Greg Turnquist
+ */
+@Component
+public class EmployeeResourceProcessor implements ResourceProcessor<Resource<Employee>> {
+
+	private static final Logger log = LoggerFactory.getLogger(EmployeeResourceProcessor.class);
+
+	@Override
+	public Resource<Employee> process(Resource<Employee> resource) {
+
+		log.info("Checking out " + resource);
+
+		Resource<Employee> newResource = new Resource<>(resource.getContent(), resource.getLink(Link.REL_SELF));
+
+		log.info("Altered as " + newResource);
+
+		return newResource;
+	}
+}
